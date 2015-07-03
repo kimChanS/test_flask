0a395560eb2fffa2bfa50fe769a0d068d2096733
KimChanS <120831629@qq.com>
2015-07-03 17:48:33 +0800

    use flask-user

--- app/user/models.py
+++ app/user/models.py
@@ -1,15 +1,26 @@
 # coding:utf8
-from flask.ext.login import UserMixin
-from app import db
+from flask_user import UserMixin, SQLAlchemyAdapter, UserManager
+from app import app, db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # User Authentication information
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, unique=True)
+    reset_password_token = db.Column(db.String(100), nullable=False, default='')
 
-    def __repr__(self):
-        return "<User %s>" % self.username
+    # User Email information
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
+
+    # User information
+    is_enabled = db.Column(db.Boolean(), nullable=False, default=False)
+    first_name = db.Column(db.String(50), nullable=False, default='')
+    last_name = db.Column(db.String(50), nullable=False, default='')
+
+    def is_active(self):
+        return self.is_enabled
+
+db_adapter = SQLAlchemyAdapter(db, User)
+user_manager = UserManager(db_adapter, app)
--- config.py
+++ config.py
@@ -5,3 +5,12 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'test.db')
 
 SECRET_KEY = 'sdfkjwlerjwejr'
+
+# mail
+MAIL_USERNAME = '120831629'
+MAIL_PASSWORD = 'password'
+MAIL_DEFAULT_SENDER = '"Sender" <120831629@qq.com>'
+MAIL_SERVER = 'smtp.qq.com'
+MAIL_PORT = 465
+MAIL_USER_SSL = True
+MAIL_USER_TLS = False
